4bac8247f257754aaa962d036f802cb233533be6
karuta0825 <magune3g@gmail.com>
2022-04-20 21:03:41 +0900

    add next-jdbc integration

--- next-jdbc/build.clj
+++ next-jdbc/build.clj
@@ -5,20 +5,18 @@
     ;; for b/git-count-revs
     [org.corfield.build :as bb]))
 
-
 (def lib 'org.clojars.karuta/sentry-next-jdbc)
-(def version "0.1.0")
-
+(def version "0.1.4")
 
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
-
 (defn test
   "Run the tests."
   [opts]
-  (bb/run-tests opts))
-
+  ;; (bb/run-tests opts)
+  (-> (merge {:main-args ["-m" "kaocha.runner"]} opts)
+      (bb/run-tests)))
 
 (defn ci
   "Run the CI pipeline of tests (and build the JAR)."
@@ -29,14 +27,12 @@
       (bb/clean)
       (bb/jar)))
 
-
 (defn install
   "Install the JAR locally."
   [opts]
   (-> opts
       (assoc :lib lib :version version)
       (bb/install)))
-
 
 (defn deploy
   "Deploy the JAR to Clojars."
